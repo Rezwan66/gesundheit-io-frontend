24fbf7713363bcb755a3584283f3fd410773edd1
Rezwan66 <shaikhrezwan66@gmail.com>
2026-03-17 04:53:11 +0100

    add dashbaord stats page and doctors management table

--- src/app/(dashboardLayout)/admin/dashboard/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/page.tsx
@@ -1,29 +1,29 @@
-import AdminDashboardContent from "@/components/modules/Dashboord/AdminDashboardContent";
-import { getDashboardData } from "@/services/dashboard.services";
-import { ApiResponse } from "@/types/api.types";
-import { IAdminDashboardData } from "@/types/dashboard.types";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import AdminDashboardContent from '@/components/modules/Dashboord/AdminDashboardContent';
+import { getDashboardData } from '@/services/dashboard.services';
+import { ApiResponse } from '@/types/api.types';
+import { IAdminDashboardData } from '@/types/dashboard.types';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 const AdminDashboardPage = async () => {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["admin-dashboard-data"],
+    queryKey: ['admin-dashboard-data'],
     queryFn: getDashboardData,
     staleTime: 30 * 1000, // 30 seconds - data stays fresh if this data is accessed again within 30 seconds, it will use the cached data instead of making a new request
     gcTime: 5 * 60 * 1000, // 5 minutes - garbage collection time, after this time the cached data will be removed from memory if it's not used
   });
 
+  //^ If we did not need any interactivity with the data, we could use the following code to get the data directly from the tanstack without using the QueryClient hook, and displayed the data directly from the page component
+  // const dashboardData = queryClient.getQueryData(["admin-dashboard-data"]) as ApiResponse<IAdminDashboardData>;
 
-  const dashboardData = queryClient.getQueryData(["admin-dashboard-data"]) as ApiResponse<IAdminDashboardData>;
-
-  console.log(dashboardData.data, "Dashboard Data from Page Component");
+  // console.log(dashboardData.data, "Dashboard Data from Page Component");
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-        <AdminDashboardContent/>
+      <AdminDashboardContent />
     </HydrationBoundary>
-  )
-}
+  );
+};
 
-export default AdminDashboardPage
+export default AdminDashboardPage;
